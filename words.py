51b9a204561375343d6831407fde80754e4a6ca8
Maxim Sheshukow <maxim.sheshukow@dptechnology.com>
2021-08-27 19:36:28 +0200

    Add json words

--- words.py
+++ words.py
@@ -1,15 +1,29 @@
-f = open("other.txt", "rt")
+import json
+
+fileName = "res/verbs"
+
+f = open(fileName + ".txt", "rt")
 words = f.readlines()
 f.close()
+
+jsonWords = []
 for i in range(len(words)):
     word = words[i]
     if word == "":
         continue
 
-    word = word.split("\t")
-    word = "|".join(word)
-    words[i] = word
+    parts = word.split("|")
+    for j in range(len(parts)):
+        parts[j] = parts[j].strip()
 
-f = open("other2.txt", "wt")
-f.writelines(words)
+    jsonWords.append({"word": parts[0], "translation": parts[1]})
+
+    # nouns
+    # Praise|Das Lob|Das Lob
+    # article = parts[1][:3]
+    # jsonWords.append(
+    #     {"article": article, "word": parts[1][4:], "plural": parts[2], "translation": parts[0]})
+
+f = open(fileName + "_json.txt", "wt")
+f.writelines(json.dumps(jsonWords))
 f.close()
